09b4b2ff3da8d510e7dbb05cc7828c1441729320
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-02-19 11:29:01 -0500

    actualización

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,37 +4,39 @@ import "../stylesheets/User.css"
 export const User = () => {
   return (
     <>
-    
-    
-     <div class="card">
 
-   
-    <div class="card__header">
-      <div class="avatar-wrap">
-        
-        <img
-          src="https://i.pravatar.cc/150?img=12"
-          alt="Jeremy Robson"
-        />
-        <span class="avatar-dot"></span>
+
+      <div class="card">
+
+        <div className="ji">
+           <div class="card__header">
+          <div class="avatar-wrap">
+
+            <img
+              src="https://i.pravatar.cc/150?img=12"
+              alt="Jeremy Robson"
+            />
+            <span class="avatar-dot"></span>
+          </div>
+
+          <p class="card__label">Report for</p>
+          <h1 class="card__name">Jeremy Robson</h1>
+        </div>
+        </div>
+       
+
+
+        <div class="card__body">
+          <nav class="period-nav" aria-label="Report period">
+            <span class="period-nav__item" onclick="setActive(this)">Daily</span>
+            <span class="period-nav__item active" onclick="setActive(this)">Weekly</span>
+            <span class="period-nav__item" onclick="setActive(this)">Monthly</span>
+          </nav>
+        </div>
+
       </div>
 
-      <p class="card__label">Report for</p>
-      <h1 class="card__name">Jeremy Robson</h1>
-    </div>
 
-    
-    <div class="card__body">
-      <nav class="period-nav" aria-label="Report period">
-        <span class="period-nav__item" onclick="setActive(this)">Daily</span>
-        <span class="period-nav__item active" onclick="setActive(this)">Weekly</span>
-        <span class="period-nav__item" onclick="setActive(this)">Monthly</span>
-      </nav>
-    </div>
-
-  </div>
-    
-    
     </>
   )
 }
