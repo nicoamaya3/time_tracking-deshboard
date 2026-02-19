a409df19c6295980b3d92dde11d5932130a9e6ea
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-02-19 11:15:20 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react'
 import './App.css'
-import {User} from './components/User'
-import {Cartas} from './components/Cartas'
+import { User } from './components/User'
+import { Cartas } from './components/Cartas'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <User></User>
-    <Cartas/>
-    <Cartas/>
-    <Cartas/>
-    <Cartas/>
-    <Cartas/>
-    <Cartas/>
+      <User></User>
+      <div className='cartass'>
+        <Cartas nombre = "Work" horas = "32 hrs" anterior="Last Week " horap="- 36hrs" color="work" />
+        <Cartas nombre = "Exercise" horas = "4 hrs" anterior="Last Week " horap="- 5hrs" color="exercise" />
+       
+      </div>
+      <div className='cartass'>
+        <Cartas nombre = "Play" horas = "10 hrs" anterior="Last Week " horap="- 8hrs" color="play" />
+        <Cartas nombre = "Social" horas = "5 hrs" anterior="Last Week " horap="- 10hrs" color="social" />
+       
+      </div>
+      <div className='cartass'>
+         <Cartas nombre = "Study" horas = "4 hrs" anterior="Last Week " horap="- 7hrs" color="study" />
+          <Cartas nombre = "Self Care" horas = "2 hrs" anterior="Last Week " horap="- 2hrs" color="selfcare" />
+      </div>
+
+
 
     </>
   )
--- src/components/Cartas.jsx
+++ src/components/Cartas.jsx
@@ -3,21 +3,21 @@
 import "../stylesheets/Cartas.css"
 
 
-export const Cartas = () => {
+export const Cartas = ({nombre , horas , anterior , horap , color}) => {
     return (
         <>
-        <div className="si">
+        <div className={`cartacolor ${color}`} >
                <div class="cards">
                 <div class="card-headers">
-                    <span class="card-title">Work</span>
+                    <span class="card-title">{nombre}</span>
                     <button class="card-menu" aria-label="More options">
                         <span></span><span></span><span></span>
                     </button>
                 </div>
-                <div class="card-hours">32hrs</div>
+                <div class="card-hours">{horas}</div>
                 <div class="card-footer">
-                    <span class="label">Last Week</span>
-                    <span class="value">–34hrs</span>
+                    <span class="label">{anterior}</span>
+                    <span class="value">{horap}</span>
                 </div>
             </div>
         </div>
